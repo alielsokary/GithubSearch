ba59d2c863b9ce5e1a6437c1ab29017da845e028
Ali Elsokary <ali.elsokary8@gmail.com>
2021-07-08 12:25:01 +0200

    Add Project

---
+++ GithubSearch/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  GithubSearch
+//
+//  Created by Ali Elsokary on 08/07/2021.
+//  
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+		return true
+	}
+
+}
+
---
+++ GithubSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubSearch
+//
+//  Created by Ali Elsokary on 08/07/2021.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
